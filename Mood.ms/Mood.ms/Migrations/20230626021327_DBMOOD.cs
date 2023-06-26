8c0364e43e9155b4806b45147648be64f8444f13
Rafa Tamura <rafaelatamura@gmail.com>
2023-06-25 23:15:06 -0300

    Config Migrations e DataBase

---
+++ Mood.ms/Mood.ms/Migrations/20230626021327_DBMOOD.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mood.ms.Migrations
+{
+    /// <inheritdoc />
+    public partial class DBMOOD : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Musica",
+                columns: table => new
+                {
+                    MusicaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MusicaNome = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    MusicaArtista = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    MusicaEstilo = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    MusicaFeeling = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    MusicaLink = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Musica", x => x.MusicaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Senha = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Musica");
+
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
--- Mood.ms/Mood.ms/Program.cs
+++ Mood.ms/Mood.ms/Program.cs
@@ -5,15 +5,24 @@ namespace Mood.ms
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
             // Add services to the container.
-
             builder.Services.AddControllers();
+            builder.Services.AddCors();
+            builder.Services.AddDbContext<Contexto>();
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            // Configura o cors para utilizar o Angular
+            builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+            {
+                builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+            }));
+
             var app = builder.Build();
+
+            app.UseCors(p => p.WithOrigins("*").AllowAnyMethod().AllowAnyHeader()); //cors angular
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
